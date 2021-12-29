f06d2a13d5a16f771dcdc61e443ce3759864ce64
Kuba Bednarczyk <kubabednarczyk2003@gmail.com>
2021-12-29 14:01:14 +0100

    done styling mobile view and load needed elements to js

---
+++ app/app.js
@@ -0,0 +1,16 @@
+const billInput = document.querySelector('.bill-input');
+
+const percentBtns = document.querySelectorAll('.grid__btn');
+
+const numOfPeopleInput = document.querySelector('.numOfPeople');
+
+const tipAmount = document.querySelector('.tip__amount');
+const tipTotal = document.querySelector('.tip__total');
+
+const resetBtn = document.querySelector('.calculator__reset');
+
+
+
+
+
+
